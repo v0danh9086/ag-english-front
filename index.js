1106195163e60741ef7baa600b2839f4375f6c2b
HaiPham <pthai076@gmail.com>
2025-01-12 22:19:13 +0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to English Learner</h1>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/lessons">Lessons</a>
+          <a href="/community">Community</a>
+        </nav>
+      </header>
+      <main>
+        <p>Improve your English skills with interactive lessons and quizzes!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
